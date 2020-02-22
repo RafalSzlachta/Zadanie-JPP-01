c8d8786e90df657b95e0a5b761e736dfdc889c1e
unknown <uzytkownik@o2.pl>
2020-02-22 15:14:24 +0100

    write code

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -1,7 +1,18 @@
 package pl.sda;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj swoje imie");
+        String imie = input.nextLine();
+        System.out.println("Podaj swoje nazwisko");
+        String nazwisko = input.nextLine();
+        System.out.println("Podaj swój wiek");
+        String wiek = input.nextLine();
+        System.out.println();
+        System.out.println("Twoje imie to " + imie + ", nazwisko to " + nazwisko + ", wiek to " + wiek);
 
     }
 }
